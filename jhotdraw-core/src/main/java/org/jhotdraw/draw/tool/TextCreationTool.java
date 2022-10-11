41438a2b52e51352e29db347e4ff8b8bcd335aec
Berkan Kütük <bekut20@student.sdu.dk>
2022-10-11 13:34:14 +0200

    Added FeatureEntryPoints

--- jhotdraw-core/src/main/java/org/jhotdraw/draw/tool/TextCreationTool.java
+++ jhotdraw-core/src/main/java/org/jhotdraw/draw/tool/TextCreationTool.java
@@ -7,6 +7,7 @@
  */
 package org.jhotdraw.draw.tool;
 
+import dk.sdu.mmmi.featuretracer.lib.FeatureEntryPoint;
 import org.jhotdraw.draw.figure.Figure;
 import org.jhotdraw.draw.figure.TextHolderFigure;
 import java.awt.*;
@@ -72,6 +73,7 @@ public class TextCreationTool extends CreationTool implements ActionListener {
     /**
      * Creates a new instance.
      */
+    @FeatureEntryPoint(value = "TextCreation")
     public TextCreationTool(TextHolderFigure prototype, Map<AttributeKey<?>, Object> attributes) {
         super(prototype, attributes);
     }
