946246c68bfb9fa651ba5c362e59693c6c225d91
Alexander Matzen <alexander@amatzen.dk>
2023-01-01 21:29:34 +0100

    Implement group figures

--- jhotdraw-core/src/main/java/org/jhotdraw/draw/action/grouping/strategy/GroupStrategy.java
+++ jhotdraw-core/src/main/java/org/jhotdraw/draw/action/grouping/strategy/GroupStrategy.java
@@ -1,11 +1,12 @@
 package org.jhotdraw.draw.action.grouping.strategy;
 
+import dk.sdu.mmmi.featuretracer.lib.FeatureEntryPoint;
 import org.jhotdraw.draw.DrawingView;
 import org.jhotdraw.draw.figure.CompositeFigure;
 import org.jhotdraw.draw.figure.Figure;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.Collection;
+import java.util.LinkedList;
 
 public class GroupStrategy implements GroupingStrategy {
     public static final String ID = "edit.groupSelection";
@@ -22,6 +23,26 @@ public class GroupStrategy implements GroupingStrategy {
 
     @Override
     public void performGroupingAction(DrawingView drawingView, CompositeFigure prototype, Collection<Figure> figures) {
-        throw new NotImplementedException();
+        final LinkedList<Figure> ungroupedFigures = new LinkedList<>(figures);
+        final CompositeFigure group = (CompositeFigure) prototype.clone();
+
+        groupFigures(drawingView, group, ungroupedFigures);
+    }
+
+
+    @FeatureEntryPoint(value = "Group figures")
+    public void groupFigures(DrawingView view, CompositeFigure group, Collection<Figure> figures) {
+        Collection<Figure> sorted = view.getDrawing().sort(figures);
+        int index = view.getDrawing().indexOf(sorted.iterator().next());
+        view.getDrawing().basicRemoveAll(figures);
+        view.clearSelection();
+        view.getDrawing().add(index, group);
+        group.willChange();
+        for (Figure f : sorted) {
+            f.willChange();
+            group.basicAdd(f);
+        }
+        group.changed();
+        view.addToSelection(group);
     }
 }
