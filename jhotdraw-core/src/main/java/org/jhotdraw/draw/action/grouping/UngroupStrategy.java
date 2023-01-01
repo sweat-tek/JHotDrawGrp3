bd84fcaf78bfcb198f8098cf86458dea0ed08faa
Alexander Matzen <alexander@amatzen.dk>
2023-01-01 18:34:54 +0100

    Intiate UngroupStrategy

---
+++ jhotdraw-core/src/main/java/org/jhotdraw/draw/action/grouping/UngroupStrategy.java
@@ -0,0 +1,20 @@
+package org.jhotdraw.draw.action.grouping;
+
+import org.jhotdraw.draw.DrawingView;
+import org.jhotdraw.draw.figure.CompositeFigure;
+import org.jhotdraw.draw.figure.Figure;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.Collection;
+
+public class UngroupStrategy implements GroupingStrategy {
+    @Override
+    public String getID() {
+        return null;
+    }
+
+    @Override
+    public void performGroupingAction(DrawingView drawingView, CompositeFigure compositeFigure, Collection<Figure> figures) {
+        throw new NotImplementedException();
+    }
+}
