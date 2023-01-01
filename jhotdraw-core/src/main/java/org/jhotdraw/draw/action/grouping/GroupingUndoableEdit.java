25abdcff916b7c8d8d4fb550440bb87f3b1edc92
Alexander Matzen <alexander@amatzen.dk>
2023-01-01 19:19:37 +0100

    Add UndoableEdit

---
+++ jhotdraw-core/src/main/java/org/jhotdraw/draw/action/grouping/GroupingUndoableEdit.java
@@ -0,0 +1,50 @@
+package org.jhotdraw.draw.action.grouping;
+
+import org.jhotdraw.draw.DrawingView;
+import org.jhotdraw.draw.action.grouping.strategy.GroupingStrategy;
+import org.jhotdraw.draw.figure.CompositeFigure;
+import org.jhotdraw.draw.figure.Figure;
+import org.jhotdraw.util.ResourceBundleUtil;
+
+import javax.swing.undo.AbstractUndoableEdit;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+import java.util.Collection;
+
+public class GroupingUndoableEdit extends AbstractUndoableEdit {
+    private final GroupingStrategy groupingStrategy;
+    private final DrawingView drawingView;
+    private final CompositeFigure prototype;
+    private final Collection<Figure> figures;
+
+    public GroupingUndoableEdit(
+        GroupingStrategy strategy,
+        DrawingView drawingView,
+        CompositeFigure prototype,
+        Collection<Figure> figures
+    ) {
+        super();
+        this.groupingStrategy = strategy;
+        this.drawingView = drawingView;
+        this.prototype = prototype;
+        this.figures = figures;
+    }
+
+    @Override
+    public String getPresentationName() {
+        ResourceBundleUtil labels = ResourceBundleUtil.getBundle("org.jhotdraw.draw.Labels");
+        return labels.getTextProperty(groupingStrategy.getID());
+    }
+
+    @Override
+    public void undo() throws CannotUndoException {
+        super.undo();
+        // TODO: Implement UNDO functionality
+    }
+
+    @Override
+    public void redo() throws CannotRedoException {
+        super.redo();
+        // TODO: Implement REDO functionality
+    }
+}
