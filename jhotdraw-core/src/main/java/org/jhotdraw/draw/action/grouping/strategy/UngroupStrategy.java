b2cc8c1c8853cb3fcdbe1e383a2031b451801af9
Alexander Matzen <alexander@amatzen.dk>
2023-01-01 22:19:43 +0100

    Implement ungroup strategy and fix group strategy

--- jhotdraw-core/src/main/java/org/jhotdraw/draw/action/grouping/strategy/UngroupStrategy.java
+++ jhotdraw-core/src/main/java/org/jhotdraw/draw/action/grouping/strategy/UngroupStrategy.java
@@ -2,11 +2,14 @@ package org.jhotdraw.draw.action.grouping.strategy;
 
 import org.jhotdraw.draw.DrawingView;
 import org.jhotdraw.draw.action.grouping.Grouping;
+import org.jhotdraw.draw.action.grouping.GroupingUndoableEdit;
 import org.jhotdraw.draw.figure.CompositeFigure;
 import org.jhotdraw.draw.figure.Figure;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
 import java.util.Collection;
+import java.util.LinkedList;
 
 public class UngroupStrategy implements GroupingStrategy {
     public static final String ID = "edit.ungroupSelection";
@@ -27,6 +30,28 @@ public class UngroupStrategy implements GroupingStrategy {
 
     @Override
     public void performGroupingAction(DrawingView drawingView, CompositeFigure prototype, Collection<Figure> figures) {
-        throw new NotImplementedException();
+        final CompositeFigure group = (CompositeFigure) drawingView.getSelectedFigures().iterator().next();
+        final LinkedList<Figure> ungroupedFigures = new LinkedList<>();
+        final GroupingUndoableEdit undoableEdit = new GroupingUndoableEdit(
+            this,
+            drawingView,
+            prototype,
+            figures
+        ) {
+            @Override
+            public void undo() throws CannotUndoException {
+                grouping.groupFigures(drawingView, group, ungroupedFigures);
+                super.undo();
+            }
+
+            @Override
+            public void redo() throws CannotRedoException {
+                super.redo();
+                grouping.ungroupFigures(drawingView, group);
+            }
+        };
+
+        ungroupedFigures.addAll(grouping.ungroupFigures(drawingView, group));
+        drawingView.getDrawing().fireUndoableEditHappened(undoableEdit);
     }
 }
