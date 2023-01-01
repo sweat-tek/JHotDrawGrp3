6dcc1c40691e876cb7c00eb967235478fb000c14
Alexander Matzen <alexander@amatzen.dk>
2023-01-01 18:32:47 +0100

    Replace Conditional Logic with Strategy (129)

---
+++ jhotdraw-core/src/main/java/org/jhotdraw/draw/action/grouping/GroupingStrategy.java
@@ -0,0 +1,30 @@
+package org.jhotdraw.draw.action.grouping;
+
+import org.jhotdraw.draw.DrawingView;
+import org.jhotdraw.draw.figure.CompositeFigure;
+import org.jhotdraw.draw.figure.Figure;
+
+import java.util.Collection;
+
+/**
+ * Grouping Strategy Contract
+ *
+ * @author Alexander Matzen
+ */
+public interface GroupingStrategy {
+    /**
+     * Returns the Strategy ID used for Swing ActionMap
+     *
+     * @return Strategy ID
+     */
+    String getID();
+
+    /**
+     * Performs the grouping action corresponding to the strategy
+     *
+     * @param drawingView The drawing view
+     * @param compositeFigure The composite figure
+     * @param figures The figures to be grouped (only applicable for GroupStrategy, leave empty collection else)
+     */
+    void performGroupingAction(DrawingView drawingView, CompositeFigure compositeFigure, Collection<Figure> figures);
+}
