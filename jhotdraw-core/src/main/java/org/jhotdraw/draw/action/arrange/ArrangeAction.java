49f03cfe1268b874594dc000eb2513886ba501b0
Esben Damkjær Sørensen <esben@damkjaergaard.com>
2023-01-01 21:39:34 +0100

    Copy list

--- jhotdraw-core/src/main/java/org/jhotdraw/draw/action/arrange/ArrangeAction.java
+++ jhotdraw-core/src/main/java/org/jhotdraw/draw/action/arrange/ArrangeAction.java
@@ -2,10 +2,13 @@ package org.jhotdraw.draw.action.arrange;
 
 import dk.sdu.mmmi.featuretracer.lib.FeatureEntryPoint;
 import org.jhotdraw.draw.DrawingEditor;
+import org.jhotdraw.draw.DrawingView;
 import org.jhotdraw.draw.action.AbstractSelectedAction;
+import org.jhotdraw.draw.figure.Figure;
 import org.jhotdraw.util.ResourceBundleUtil;
 
 import java.awt.event.ActionEvent;
+import java.util.LinkedList;
 
 public class ArrangeAction extends AbstractSelectedAction {
     private final ArrangeStrategy strategy;
@@ -23,8 +26,11 @@ public class ArrangeAction extends AbstractSelectedAction {
     @Override
     @FeatureEntryPoint("Arrange")
     public void actionPerformed(ActionEvent e) {
+        final DrawingView view = getView();
+        final LinkedList<Figure> figures = new LinkedList<>(view.getSelectedFigures());
+
         ArrangeActionTrigger action = new ArrangeActionTrigger(
-                strategy, getView(), getView().getSelectedFigures()
+                strategy, view, figures
         );
 
         action.performAction();
