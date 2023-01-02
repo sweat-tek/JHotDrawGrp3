5913ea68084ac8077557b7c89c2f7ca54e56d3b5
Alexander Matzen <alexander@amatzen.dk>
2023-01-02 14:21:52 +0100

    Implement first scenario test

--- jhotdraw-core/src/test/java/org/jhotdraw/draw/action/grouping/GroupingScenarioTest.java
+++ jhotdraw-core/src/test/java/org/jhotdraw/draw/action/grouping/GroupingScenarioTest.java
@@ -1,11 +1,24 @@
 package org.jhotdraw.draw.action.grouping;
 
 import com.tngtech.jgiven.junit.ScenarioTest;
-import org.junit.jupiter.api.Test;
+import org.jhotdraw.draw.figure.Figure;
+import org.jhotdraw.draw.figure.RectangleFigure;
+import org.jhotdraw.draw.figure.RoundRectangleFigure;
+import org.jhotdraw.draw.figure.TextFigure;
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.List;
 
 public class GroupingScenarioTest extends ScenarioTest<GivenGrouping, WhenGrouping, ThenGrouping> {
 
     @Test
-    void name() {
+    public void group_figures() {
+        Collection<Figure> figures = List.of(new RectangleFigure(), new TextFigure(), new RoundRectangleFigure());
+
+        given().some_ungrouped_figures(figures);
+        when().the_user_selects_the_figures(figures)
+                .and().the_user_groups_the_figures();
+        then().the_editor_contains_the_group_containing_the_figures(figures);
     }
 }
