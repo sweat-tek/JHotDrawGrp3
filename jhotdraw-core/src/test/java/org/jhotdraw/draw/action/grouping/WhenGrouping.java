6ddafc5f043876e5df81586340d20bc687bc9290
Alexander Matzen <alexander@amatzen.dk>
2023-01-02 13:46:56 +0100

    Group action mock

--- jhotdraw-core/src/test/java/org/jhotdraw/draw/action/grouping/WhenGrouping.java
+++ jhotdraw-core/src/test/java/org/jhotdraw/draw/action/grouping/WhenGrouping.java
@@ -6,7 +6,10 @@ import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import org.jhotdraw.draw.DrawingEditor;
 import org.jhotdraw.draw.figure.Figure;
 
+import java.awt.event.ActionEvent;
 import java.util.Collection;
+
+import static org.mockito.Mockito.mock;
 
 public class WhenGrouping extends Stage<WhenGrouping> {
     @ExpectedScenarioState
@@ -15,6 +18,16 @@ public class WhenGrouping extends Stage<WhenGrouping> {
 
     public WhenGrouping the_user_selects_the_figures(Collection<Figure> figures) {
         figures.forEach(editor.getActiveView()::addToSelection);
+        return self();
+    }
+
+    public WhenGrouping the_user_groups_the_figures() {
+        GroupingAction action = GroupingActionFactory.initiate(editor)
+            .withGroupingStrategy(new GroupStrategy())
+            .create();
+
+        action.actionPerformed(mock(ActionEvent.class));
+
         return self();
     }
 
