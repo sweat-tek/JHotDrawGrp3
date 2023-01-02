6ae734092b1c1ca266b7326ee0f598cf94456293
Alexander Matzen <alexander@amatzen.dk>
2023-01-02 14:21:25 +0100

    Implement first test's then stage

--- jhotdraw-core/src/test/java/org/jhotdraw/draw/action/grouping/ThenGrouping.java
+++ jhotdraw-core/src/test/java/org/jhotdraw/draw/action/grouping/ThenGrouping.java
@@ -1,7 +1,31 @@
 package org.jhotdraw.draw.action.grouping;
 
 import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import org.jhotdraw.draw.DrawingEditor;
+import org.jhotdraw.draw.figure.Figure;
+import org.jhotdraw.draw.figure.GroupFigure;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.Collection;
 
 public class ThenGrouping extends Stage<ThenGrouping> {
+
+    @ExpectedScenarioState
+    private DrawingEditor editor;
+
+    @ExpectedScenarioState
+    private Collection<Figure> selection;
+
+    public ThenGrouping the_editor_contains_the_group_containing_the_figures(Collection<Figure> figures) {
+        Assertions.assertEquals(1, selection.size());
+
+        GroupFigure group = (GroupFigure) selection.iterator().next();
+        Assertions.assertEquals(figures.size(), group.getChildCount());
+
+        figures.forEach(f -> Assertions.assertTrue(group.contains(f)));
+
+        return self();
+    }
 
 }
