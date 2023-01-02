1a464d8e129157e214cae3299833cd099cac6494
Alexander Matzen <alexander@amatzen.dk>
2023-01-02 13:46:10 +0100

    Implement the_user_groups_the_figures

--- jhotdraw-core/src/test/java/org/jhotdraw/draw/action/grouping/WhenGrouping.java
+++ jhotdraw-core/src/test/java/org/jhotdraw/draw/action/grouping/WhenGrouping.java
@@ -4,10 +4,18 @@ import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import org.jhotdraw.draw.DrawingEditor;
+import org.jhotdraw.draw.figure.Figure;
+
+import java.util.Collection;
 
 public class WhenGrouping extends Stage<WhenGrouping> {
     @ExpectedScenarioState
     @ProvidedScenarioState
     private DrawingEditor editor;
+
+    public WhenGrouping the_user_selects_the_figures(Collection<Figure> figures) {
+        figures.forEach(editor.getActiveView()::addToSelection);
+        return self();
+    }
 
 }
