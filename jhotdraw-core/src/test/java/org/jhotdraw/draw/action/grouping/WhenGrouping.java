1345236c61d1aaba9b833fe4bef4a321bc9eb68c
Alexander Matzen <alexander@amatzen.dk>
2023-01-02 13:46:00 +0100

    Grab Editor

--- jhotdraw-core/src/test/java/org/jhotdraw/draw/action/grouping/WhenGrouping.java
+++ jhotdraw-core/src/test/java/org/jhotdraw/draw/action/grouping/WhenGrouping.java
@@ -1,7 +1,13 @@
 package org.jhotdraw.draw.action.grouping;
 
 import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+import org.jhotdraw.draw.DrawingEditor;
 
 public class WhenGrouping extends Stage<WhenGrouping> {
+    @ExpectedScenarioState
+    @ProvidedScenarioState
+    private DrawingEditor editor;
 
 }
