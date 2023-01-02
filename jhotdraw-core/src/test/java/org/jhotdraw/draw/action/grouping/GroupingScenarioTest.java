3f8da77cbb8b74eb4c51f0fb3eda559dbbd0b3cc
Alexander Matzen <alexander@amatzen.dk>
2023-01-02 12:48:43 +0100

    Add ScenarioTest

---
+++ jhotdraw-core/src/test/java/org/jhotdraw/draw/action/grouping/GroupingScenarioTest.java
@@ -0,0 +1,11 @@
+package org.jhotdraw.draw.action.grouping;
+
+import com.tngtech.jgiven.junit.ScenarioTest;
+import org.junit.jupiter.api.Test;
+
+public class GroupingScenarioTest extends ScenarioTest<GivenGrouping, WhenGrouping, ThenGrouping> {
+
+    @Test
+    void name() {
+    }
+}
